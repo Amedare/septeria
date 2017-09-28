5a4ffbbf35adc349298da7f4761b8689de571d94
Dmitriy <nikersen@gmail.com>
2017-09-28 11:30:21 +0300

    Youtube player on

--- js/main.js
+++ js/main.js
@@ -73,7 +73,7 @@ $('.js-single-shine').click(function(){
 
 // Video manage
 var tag = document.createElement('script');
-// tag.src = "https://www.youtube.com/player_api";
+tag.src = "https://www.youtube.com/player_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
